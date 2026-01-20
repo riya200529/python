010a469d34e7c313ff1b235b8d42e76574aedee5
riya kukadiya <riyakukadiya589@gmail.com>
2026-01-20 10:59:18 +0530

    Add files via upload

---
+++ PRACTIC/ex3_op.py
@@ -0,0 +1,19 @@
+#increment num1 by 10
+num1=int(input("enter num1:"))
+num1+=10
+print(f"{num1}")
+
+#decrement num2 by 20
+num2=int(input("enter num2:"))
+num2-=20
+print(f"{num2}")
+
+#multiply num3 by 2
+num3=int(input("enter num3:"))
+num3*=2
+print(f"{num3}")
+
+#divison num4 by 5
+num4=int(input("enter num4:"))
+num4/=5
+print(f"{num4}")
